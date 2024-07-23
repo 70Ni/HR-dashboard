36666bd417184194c013cce54ea13d19a48da9fc
70ni <4thedevelopments@gmail.com>
2024-07-23 20:13:45 +0530

    Added Data for sumCard

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import SumCard from "./Components/Cards/SummaryCard/SumCard";
+import colors from "./api/colors.json";
+
+const sumData = [
+  {
+    header: "Available Position",
+    count: "24",
+    notificaton: "4 Urgently needed",
+    colors: {
+      bg: colors.primary.light,
+      textColor: colors.primary.base,
+      //change colors from colors.json
+    },
+  },
+  {
+    header: "Job Open",
+    count: "10",
+    notificaton: "4 Active hiring",
+    colors: {
+      bg: colors.tertiary.light,
+      textColor: colors.tertiary.base,
+    },
+  },
+  {
+    header: "New Employees",
+    count: "24",
+    notificaton: "4 Department",
+    colors: {
+      bg: colors.pink.light,
+      textColor: colors.pink.base,
+    },
+  },
+];
+
+sumData.map((x) => console.log(x.colors));
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className=" App flex justify-center items-center h-full">
+      {sumData.map((x, i) => (
+        <SumCard
+          header={x.header}
+          count={x.count}
+          notificaton={x.notificaton}
+          color={x.colors}
+          key={i}
+        />
+      ))}
     </div>
   );
 }
