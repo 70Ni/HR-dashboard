65c41b764cf23ad40326f4570fd6d779f1bbaaf3
70ni <4thedevelopments@gmail.com>
2024-07-23 20:13:28 +0530

    SumCard iterate with data

---
+++ src/Components/Cards/SummaryCard/SumCard.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import colors from "../../../api/colors.json";
+
+function SumCard({ color, header, count, notificaton, key }) {
+  return (
+    <div
+      key={key}
+      className="px-5 py-3 min-w-[204px]  rounded-default"
+      style={{ backgroundColor: color.bg }}
+    >
+      <div className="content flex flex-col items-start ">
+        <div className="inicator text-titilesmall ">{header}</div>
+        <div className="indicator-number text-count py-4 font-medium">
+          {count}
+        </div>
+        <div
+          className="indi-note text-Indicator leading-6"
+          style={{ color: color.textColor }}
+        >
+          {notificaton}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default SumCard;
