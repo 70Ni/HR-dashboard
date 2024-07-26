95958fdab2fa0f9305455938a61b52606a46ac3b
70ni <4thedevelopments@gmail.com>
2024-07-26 09:26:44 +0530

    Task card to props Data (patch)

--- src/Components/Cards/TaskCard/TaskCard.jsx
+++ src/Components/Cards/TaskCard/TaskCard.jsx
@@ -2,19 +2,19 @@ import React from "react";
 import moreIcon from "../../../Image/Icons/more.svg";
 import Pin from "../../../Image/Icons/Pin.svg";
 
-function TaskCard() {
+function TaskCard({ task, time }) {
   return (
-    <div className="outer border border-borderColor outline-offset-1 px-4 py-3 bg-taskBgColor rounded-md">
-      <div className="inner-section flex">
-        <div className="inner ">
+    <div className="outer border border-borderColor outline-offset-1  bg-taskBgColor rounded-md">
+      <div className="inner-section flex justify-between px-4 py-3 w-full items-center">
+        <div className="inner w-fit">
           <div className="text-taskHeader text-left text-SecoTitleColor pb-[4px]">
-            Meeting HR Department
+            {task}
           </div>
           <div className="text-bodySmall text-left text-primSubTitleColor">
-            Yesterday, 12:30 PM
+            {time}
           </div>
         </div>
-        <div className="icons-wrapper gap-5 flex ml-4 items-center">
+        <div className="icons-wrapper gap-5 ml-4 flex">
           <img
             src={Pin}
             alt=""
